d4bf7a8cd3c9d0bfae12dbe5a2ee70d874db284b
David Sisco <dsisco11@gmail.com>
2023-02-22 01:31:58 -0800

    Add proper debug views for token structures

--- SnapshotTests/Snapshots/compound_tokens/Generated#MetaParser.MetaParser.compound_tokens.token.struct.g.verified.cs
+++ SnapshotTests/Snapshots/compound_tokens/Generated#MetaParser.MetaParser.compound_tokens.token.struct.g.verified.cs
@@ -1,17 +1,25 @@
 ﻿//HintName: MetaParser.MetaParser.compound_tokens.token.struct.g.cs
 namespace Foo.Bar.Tokens;
+[System.Diagnostics.DebuggerDisplay("{Data}", Name = "{(ETokenType)Id}")]
 public readonly record struct ValueToken(byte Id, global::System.ReadOnlyMemory<char> Data)
 {
     public override string ToString()
     {
-        return $"{(ETokenType)Id}({Data})";
+        return Data.ToString();
     }
 }
 
+[System.Diagnostics.DebuggerDisplay("{this.ToString()}", Name = "{(ETokenType)Id}")]
 public sealed record Token(ETokenType Id, ValueToken[] Values)
 {
     public override string ToString()
     {
-        return $"{Id}[{String.Join(", ", Values.Select(o => o.ToString()))}]";
+        var sb = new global::System.Text.StringBuilder();
+        for (int i=0; i<Values.Length; i++)
+        {
+            sb.Append(Values[i].Data.ToString());
+        }
+        
+        return sb.ToString();
     }
 }
