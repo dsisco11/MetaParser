773612c3bf0401db71dd0dc1d48ececa0916ed1e
David Sisco <dsisco11@gmail.com>
2023-02-25 23:54:24 -0800

    temporarily comment out old deps graph

--- MetaParser/DependencyGraph/DepsGraph.cs
+++ MetaParser/DependencyGraph/DepsGraph.cs
@@ -1,70 +1,65 @@
-﻿using MetaParser.Schemas.Structs;
-using System.Collections.Generic;
-using System.Collections.Immutable;
-using System.Linq;
-
-namespace MetaParser.DependencyGraph
+﻿namespace MetaParser.DependencyGraph
 {
-    internal static class DepsGraph
-    {
-        public static Dictionary<string, DependencyNode<TokenDefComplex>> Build(IEnumerable<TokenDefComplex> Tokens)
-        {
-            var dependencies = Tokens.ToDictionary(tok => tok.Name!, tok => new DependencyNode<TokenDefComplex>(tok.Name!, tok));
+    //internal static class DepsGraph
+    //{
+    //    public static Dictionary<string, DependencyNode<string>> Build(IEnumerable<TokenDefComplex> Tokens)
+    //    {
+    //        var dependencies = Tokens.ToDictionary(tok => tok.Name!, tok => new DependencyNode<TokenDefComplex>(tok.Name!, tok));
 
-            // Foreach complex token, add all of the other tokens which it references to its dependency node
-            foreach (var token in Tokens)
-            {
-                if (dependencies.TryGetValue(token.Name!, out var tokDep))
-                {
-                    // Add all pattern-start tokens
-                    foreach (var subTok in token.Start)
-                    {
-                        // find this tokens dependency node so we can link it
-                        if (dependencies.TryGetValue(subTok, out var subTokDep))
-                        {
-                            tokDep.Add(subTokDep);
-                        }
-                    }
+    //        // Foreach complex token, add all of the other tokens which it references to its dependency node
+    //        foreach (var token in Tokens)
+    //        {
+    //            if (dependencies.TryGetValue(token.Name!, out var tokDep))
+    //            {
+    //                // Add all pattern-start tokens
+    //                foreach (var subTok in token.Start)
+    //                {
+    //                    // find this tokens dependency node so we can link it
+    //                    if (dependencies.TryGetValue(subTok, out var subTokDep))
+    //                    {
+    //                        tokDep.Add(subTokDep);
+    //                    }
+    //                }
 
-                    // Add all consumed tokens
-                    foreach (var subTok in token.Consume)
-                    {
-                        // find this tokens dependency node so we can link it
-                        if (dependencies.TryGetValue(subTok, out var subTokDep))
-                        {
-                            tokDep.Add(subTokDep);
-                        }
-                    }
+    //                // Add all consumed tokens
+    //                foreach (var subTok in token.Consume)
+    //                {
+    //                    // find this tokens dependency node so we can link it
+    //                    if (dependencies.TryGetValue(subTok, out var subTokDep))
+    //                    {
+    //                        tokDep.Add(subTokDep);
+    //                    }
+    //                }
 
-                    if (token.Stop is not null)
-                    {
-                        // Add all pattern-terminator tokens
-                        foreach (var subTok in token.Stop)
-                        {
-                            // find this tokens dependency node so we can link it
-                            if (dependencies.TryGetValue(subTok, out var subTokDep))
-                            {
-                                tokDep.Add(subTokDep);
-                            }
-                        }
+    //                if (token.Stop is not null)
+    //                {
+    //                    // Add all pattern-terminator tokens
+    //                    foreach (var subTok in token.Stop)
+    //                    {
+    //                        // find this tokens dependency node so we can link it
+    //                        if (dependencies.TryGetValue(subTok, out var subTokDep))
+    //                        {
+    //                            tokDep.Add(subTokDep);
+    //                        }
+    //                    }
 
-                        if (token.Escape is not null)
-                        {
-                            // Add all pattern-terminator-escape tokens
-                            foreach (var subTok in token.Escape)
-                            {
-                                // find this tokens dependency node so we can link it
-                                if (dependencies.TryGetValue(subTok, out var subTokDep))
-                                {
-                                    tokDep.Add(subTokDep);
-                                }
-                            }
-                        }
+    //                    if (token.Escape is not null)
+    //                    {
+    //                        // Add all pattern-terminator-escape tokens
+    //                        foreach (var subTok in token.Escape)
+    //                        {
+    //                            // find this tokens dependency node so we can link it
+    //                            if (dependencies.TryGetValue(subTok, out var subTokDep))
+    //                            {
+    //                                tokDep.Add(subTokDep);
+    //                            }
+    //                        }
+    //                    }
 
-                    }
-                }
-            }
-            return dependencies;
-        }
-    }
+    //                }
+    //            }
+    //        }
+    //        return dependencies;
+    //    }
+    //}
 }
