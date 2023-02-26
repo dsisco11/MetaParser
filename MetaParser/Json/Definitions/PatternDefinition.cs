cb2179379066a393cc4b7bba5fe9976ba5616096
David Sisco <dsisco11@gmail.com>
2023-02-26 04:31:54 -0800

    remove $type property reference from PatternDefinition

--- MetaParser/Json/Definitions/PatternDefinition.cs
+++ MetaParser/Json/Definitions/PatternDefinition.cs
@@ -1,22 +1,9 @@
 ﻿using MetaParser.Contexts;
-using MetaParser.Json.JsonTypeConverters;
 using MetaParser.Patternization;
-using System.Collections.Generic;
-using System.Text.Json.Serialization;
 
 namespace MetaParser.Json.Definitions;
 
-[JsonPolymorphic]
-[JsonDerivedType(typeof(ValuePattern), typeDiscriminator: "constant")]
-[JsonDerivedType(typeof(TokenPattern), typeDiscriminator: "compound")]
 internal abstract record PatternDefinition
 {
-    [JsonPropertyName("$type")]
-    public ETokenType Type { get; set; }
     public abstract Pattern Resolve(MetaParserContext context);
 }
-
-//[JsonConverter(typeof(JsonOneToManyDictionaryConverter))]
-//internal class PatternDefinitionList : List<PatternDefinition>
-//{
-//}
