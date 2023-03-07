571017ebef4438ec44cb364219d14512d549bf0f
David Sisco <dsisco11@gmail.com>
2023-03-07 15:40:07 -0800

    update unit tests

---
+++ SnapshotTests/Snapshots/linear_parser/Generated#MetaParser.MetaParser.linear_parser.tokens.complex.g.received.cs
@@ -0,0 +1,17 @@
+﻿//HintName: MetaParser.MetaParser.linear_parser.tokens.complex.g.cs
+namespace UnitTestParser
+{
+    public sealed partial class Parser
+    {
+        private static bool TryProcessComplex(global::System.ReadOnlySpan<byte> input, out byte id, out int length)
+        {
+            switch (input)
+            {
+            }
+            id = default;
+            length = default;
+            return false;
+            
+        }
+    }
+}
