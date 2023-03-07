1d978c3aeb1038b33a46e4c5a3aba4dd534682a0
David Sisco <dsisco11@gmail.com>
2023-03-07 13:24:03 -0800

    start moving to more flexible system for building code based on config options

---
+++ MetaParser/CodeGen/CodeBuilderProviderFactory.cs
@@ -0,0 +1,26 @@
+﻿using MetaParser.CodeGen.Interfaces;
+using MetaParser.Contexts;
+
+namespace MetaParser.CodeGen;
+
+internal abstract class CodeBuilderProviderFactory
+{
+    #region Properties
+    protected readonly MetaParserContext Context;
+    #endregion
+
+    #region Constructors
+    public CodeBuilderProviderFactory(MetaParserContext context)
+    {
+        Context = context;
+    }
+    #endregion
+
+    public abstract IMetaCodeBuilder Get_Parsing_Logic();
+    public abstract IMetaCodeBuilder Get_Token_Struct_Builder();
+    public abstract IMetaCodeBuilder Get_Token_ID_Enum_Builder();
+    public abstract IMetaCodeBuilder Get_Token_ID_Constants_Builder();
+    public abstract IMetaCodeBuilder Get_Token_Detection_Logic();
+    public abstract IMetaCodeBuilder Get_Token_Processing_Logic();
+    public abstract IMetaCodeBuilder Get_Token_Consumer_Logic();
+}
