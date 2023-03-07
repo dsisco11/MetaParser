b334b33e563c98795c0a6276ac2235c7ee966a34
David Sisco <dsisco11@gmail.com>
2023-03-07 13:31:21 -0800

    remove obsolete members

--- MetaParser/CodeGen/CodeCommon.cs
+++ MetaParser/CodeGen/CodeCommon.cs
@@ -23,37 +23,5 @@ namespace MetaParser.CodeGen
         public const string ReadOnlyMemory = "global::System.ReadOnlyMemory";
         #endregion
 
-        [Obsolete]
-        public static string FormatMemoryBuffer(SpecialType type) => $"global::System.Memory<{Format(type)}>";
-        [Obsolete]
-        public static string FormatSpanBuffer(SpecialType type) => $"global::System.Span<{Format(type)}>";
-
-        [Obsolete]
-        public static string FormatReadOnlyMemoryBuffer(SpecialType type) => $"global::System.ReadOnlyMemory<{Format(type)}>";
-        [Obsolete]
-        public static string FormatReadOnlySpanBuffer(SpecialType type) => $"global::System.ReadOnlySpan<{Format(type)}>";
-
-        [Obsolete]
-        public static string Format(SpecialType type)
-        {
-            return type switch
-            {
-                SpecialType.System_SByte => "sbyte",
-                SpecialType.System_Byte => "byte",
-                SpecialType.System_Char => "char",
-                SpecialType.System_Int16 => "short",
-                SpecialType.System_Int32 => "int",
-                SpecialType.System_Int64 => "long",
-                SpecialType.System_UInt16 => "ushort",
-                SpecialType.System_UInt32 => "uint",
-                SpecialType.System_UInt64 => "ulong",
-                SpecialType.System_Single => "float",
-                SpecialType.System_Double => "double",
-                SpecialType.System_String => "string",
-                SpecialType.System_Void => "void",
-                SpecialType.System_Boolean => "bool",
-                _ => throw new NotImplementedException()
-            };
-        }
     }
 }
