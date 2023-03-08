36aa7474c0eb30c8f5ed99cfa2293409476e810b
David Sisco <dsisco11@gmail.com>
2023-03-08 06:44:23 -0800

    prepare for recursive consumer logic

--- MetaParser/Builders/TokenLogic/Consumer/DetectTokensAndThen.cs
+++ MetaParser/Builders/TokenLogic/Consumer/DetectTokensAndThen.cs
@@ -11,33 +11,47 @@ internal class DetectTokensAndThen : MetaCodeBuilder
     // TODO: Detect cyclic tokens and generate different detection & consuming logic
     protected override void Write(MetaParserContext context)
     {
+        var writer = context.writer;
         var workTokens = context.Consumers with { WorkingSet = new TokenConsumer[1] };
         var workContext = context with { Consumers = workTokens };
 
-        var sortedConsumers = context.Consumers.WorkingSet.OrderByDescending(static (c) => c.DependencyInfo.MaxDepth).ThenByDescending(static (c) => c.Start.Length);
-
-        var writer = context.writer;
-        writer.WriteLine($"switch ({VarNameBufferMajor})");
-        writer.WriteLine("{");
-        writer.Indent++;
-
-        foreach (TokenConsumer consumer in sortedConsumers)
+        #region Recursive Consumers
+        var consumersRecursive = context.Consumers.WorkingSet.Where(static c => c.DependencyInfo.IsRecursive);
+        if (consumersRecursive.Any())
         {
-            workContext.Consumers.WorkingSet[0] = consumer;
+            var sortedConsumersRecursive = consumersRecursive.OrderByDescending(static (c) => c.DependencyInfo.MaxDepth).ThenByDescending(static (c) => c.Start.Length);
 
-            writer.Write("case ");
-            ConsumerPatternFormatter.WriteTo(context, consumer.Start);
-            writer.WriteLine(":");
+        }
+        #endregion
+
+        #region Non-Recursive Consumers
+        var consumersLinear = context.Consumers.WorkingSet.Where(static c => !c.DependencyInfo.IsRecursive);
+        if (consumersLinear.Any())
+        {
+            var sortedConsumersLinear = consumersLinear.OrderByDescending(static (c) => c.DependencyInfo.MaxDepth).ThenByDescending(static (c) => c.Start.Length);
+            writer.WriteLine($"switch ({VarNameBufferMajor})");
             writer.WriteLine("{");
             writer.Indent++;
 
-            base.WriteContent(workContext);
+            foreach (TokenConsumer consumer in sortedConsumersLinear)
+            {
+                workContext.Consumers.WorkingSet[0] = consumer;
+
+                writer.Write("case ");
+                ConsumerPatternFormatter.WriteTo(context, consumer.Start);
+                writer.WriteLine(":");
+                writer.WriteLine("{");
+                writer.Indent++;
+
+                base.WriteContent(workContext);
+
+                writer.Indent--;
+                writer.WriteLine("}");
+            }
 
             writer.Indent--;
-            writer.WriteLine("}");
+            writer.WriteLine("}");// end switch
         }
-
-        writer.Indent--;
-        writer.WriteLine("}");// end switch
+        #endregion
     }
 }
---
+++ SnapshotTests/Snapshots/recursive_parser/Generated#MetaParser.MetaParser.recursive_parser.tokens.compound.g.received.cs
@@ -0,0 +1,123 @@
+﻿//HintName: MetaParser.MetaParser.recursive_parser.tokens.compound.g.cs
+namespace UnitTestParser
+{
+    public sealed partial class Parser
+    {
+        private static bool TryProcessCompound(global::System.ReadOnlySpan<byte> input, out byte id, out int length)
+        {
+            switch (input)
+            {
+            }
+            id = default;
+            length = default;
+            return false;
+            
+            bool consume_pattern_14(global::System.ReadOnlySpan<byte> input, out int length)
+            {
+                /*
+                * TokenID: comment (#13)
+                * ==[ CONSUMER_DATA ]==
+                * TokenConsumer { Token = TokenInfo { Index = 13, Name = comment, Consumers = System.Collections.Generic.List`1[MetaParser.Consumers.TokenConsumer], Identity = TokenGraphId { TokenIndex = 13, ConsumerIndex = -1 } }, Index = 14, Type = Token, Identity = TokenGraphId { TokenIndex = 13, ConsumerIndex = 14 }, DependencyInfo = ResolvedVertexNode { Order = 26, MinDepth = 1, MaxDepth = 1, IsRecursive = True }, Start = PatternGroup { Length = 2, IsRawValues = True, IsConstantLength = True, HasChildren = True, Condition = AllOf, ConditionJoiner = , , MinLength = 2 }, Consume = , Stop = PatternGroup { Length = 1, IsRawValues = True, IsConstantLength = True, HasChildren = True, Condition = AllOf, ConditionJoiner = , , MinLength = 1 }, Escape = , IsOpen = False, IsClosed = True, IsDynamic = True, IsConstant = False }
+                */
+                /* Consume the START sequence which got us here in the first place, we already know its part of the token */
+                var buffer = input.Slice(2);
+                while (buffer.Length > 0)
+                {
+                    if (buffer.StartsWith(stackalloc []{ TokenId.Newline}))
+                    /* If we have a STOP sequence, check for it */
+                    {
+                        /* end */
+                        break;
+                    }
+                    
+                    /* Token doesn't specify any explicit consumables, so ALL items are considered valid consumables */
+                    buffer = buffer.Slice(1);
+                }
+                
+                if (buffer.StartsWith(stackalloc []{ TokenId.Newline}))
+                {
+                    length = 1 + (input.Length - buffer.Length);
+                    return true;
+                }
+                
+                length = default;
+                return false;
+            }
+            bool consume_pattern_15(global::System.ReadOnlySpan<byte> input, out int length)
+            {
+                /*
+                * TokenID: declaration (#14)
+                * ==[ CONSUMER_DATA ]==
+                * TokenConsumer { Token = TokenInfo { Index = 14, Name = declaration, Consumers = System.Collections.Generic.List`1[MetaParser.Consumers.TokenConsumer], Identity = TokenGraphId { TokenIndex = 14, ConsumerIndex = -1 } }, Index = 15, Type = Token, Identity = TokenGraphId { TokenIndex = 14, ConsumerIndex = 15 }, DependencyInfo = ResolvedVertexNode { Order = 26, MinDepth = 1, MaxDepth = 1, IsRecursive = True }, Start = PatternGroup { Length = 2, IsRawValues = True, IsConstantLength = True, HasChildren = True, Condition = AllOf, ConditionJoiner = , , MinLength = 2 }, Consume = , Stop = PatternGroup { Length = 1, IsRawValues = True, IsConstantLength = True, HasChildren = True, Condition = AllOf, ConditionJoiner = , , MinLength = 1 }, Escape = , IsOpen = False, IsClosed = True, IsDynamic = True, IsConstant = False }
+                */
+                /* Consume the START sequence which got us here in the first place, we already know its part of the token */
+                var buffer = input.Slice(2);
+                while (buffer.Length > 0)
+                {
+                    if (buffer.StartsWith(stackalloc []{ TokenId.Semicolon}))
+                    /* If we have a STOP sequence, check for it */
+                    {
+                        /* end */
+                        break;
+                    }
+                    
+                    /* Token doesn't specify any explicit consumables, so ALL items are considered valid consumables */
+                    buffer = buffer.Slice(1);
+                }
+                
+                if (buffer.StartsWith(stackalloc []{ TokenId.Semicolon}))
+                {
+                    length = 1 + (input.Length - buffer.Length);
+                    return true;
+                }
+                
+                length = default;
+                return false;
+            }
+            bool consume_pattern_16(global::System.ReadOnlySpan<byte> input, out int length)
+            {
+                /*
+                * TokenID: codeblock (#15)
+                * ==[ CONSUMER_DATA ]==
+                * TokenConsumer { Token = TokenInfo { Index = 15, Name = codeblock, Consumers = System.Collections.Generic.List`1[MetaParser.Consumers.TokenConsumer], Identity = TokenGraphId { TokenIndex = 15, ConsumerIndex = -1 } }, Index = 16, Type = Token, Identity = TokenGraphId { TokenIndex = 15, ConsumerIndex = 16 }, DependencyInfo = ResolvedVertexNode { Order = 26, MinDepth = 1, MaxDepth = 1, IsRecursive = True }, Start = PatternGroup { Length = 1, IsRawValues = True, IsConstantLength = True, HasChildren = True, Condition = AllOf, ConditionJoiner = , , MinLength = 1 }, Consume = PatternGroup { Length = 1, IsRawValues = False, IsConstantLength = True, HasChildren = True, Condition = OneOf, ConditionJoiner =  or , MinLength = 1 }, Stop = PatternGroup { Length = 1, IsRawValues = True, IsConstantLength = True, HasChildren = True, Condition = AllOf, ConditionJoiner = , , MinLength = 1 }, Escape = , IsOpen = False, IsClosed = True, IsDynamic = True, IsConstant = False }
+                */
+                /* Consume the START sequence which got us here in the first place, we already know its part of the token */
+                var buffer = input.Slice(1);
+                while (buffer.Length > 0)
+                {
+                    if (buffer.StartsWith(stackalloc []{ TokenId.Close_Bracket}))
+                    /* If we have a STOP sequence, check for it */
+                    {
+                        /* end */
+                        break;
+                    }
+                    
+                    
+                    while (buffer.Length > 0)
+                    {
+                        if (buffer is [ TokenId.Declaration, ..])
+                        /* If we have a set of valid CONSUME targets, then try and consume as many as possible (STOP sequence should be mutually exclusive with CONSUME sequence) */
+                        {
+                            buffer = buffer.Slice(1);
+                            /* consumer forces moving on to next loop */
+                            continue;
+                        }
+                        
+                        /* otherwise, default behaviour is to exit loop */
+                        break;
+                    }
+                    
+                }
+                
+                if (buffer.StartsWith(stackalloc []{ TokenId.Close_Bracket}))
+                {
+                    length = 1 + (input.Length - buffer.Length);
+                    return true;
+                }
+                
+                length = default;
+                return false;
+            }
+        }
+    }
+}
