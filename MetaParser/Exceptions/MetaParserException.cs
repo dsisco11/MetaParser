913eeddbff6981754d67230aafe666d47cfe3e9f
David Sisco <dsisco11@gmail.com>
2023-03-10 09:39:10 -0800

    add generalized MetaParserException

---
+++ MetaParser/Exceptions/MetaParserException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace MetaParser.Exceptions;
+
+internal class MetaParserException : Exception
+{
+    public MetaParserException()
+    {
+    }
+
+    public MetaParserException(string message) : base(message)
+    {
+    }
+
+    public MetaParserException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+
+    protected MetaParserException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+}
