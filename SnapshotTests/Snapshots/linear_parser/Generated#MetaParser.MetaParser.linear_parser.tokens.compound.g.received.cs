00873ab1cdaa628d11282606f60565596ed6f9ca
David Sisco <dsisco11@gmail.com>
2023-03-10 18:15:47 -0800

    consolidate graphing constructs

---
+++ SnapshotTests/Snapshots/linear_parser/Generated#MetaParser.MetaParser.linear_parser.tokens.compound.g.received.cs
@@ -0,0 +1,14 @@
+﻿//HintName: MetaParser.MetaParser.linear_parser.tokens.compound.g.cs
+namespace UnitTestParser
+{
+    public sealed partial class Parser
+    {
+        private static bool TryProcessCompound(global::System.ReadOnlySpan<byte> input, out byte id, out int length)
+        {
+            id = default;
+            length = default;
+            return false;
+            
+        }
+    }
+}
