77df016af23070af74acd86a7a89e0d17d0860f0
David Sisco <dsisco11@gmail.com>
2023-03-16 14:34:40 -0700

    update snapshot parser definitions

--- SnapshotTests/Tests/Generation.cs
+++ SnapshotTests/Tests/Generation.cs
@@ -13,8 +13,7 @@ public class Generation : IClassFixture<CodeGeneratorFixture>
     }
 
     [Theory]
-    [InlineData("linear_parser.json")]
-    [InlineData("recursive_parser.json")]
+    [InlineData("parser.json")]
     public Task Result(string fileName)
     {
         var sourceCode = CodeGeneratorFixture.Get_Input_File_Contents(fileName);
