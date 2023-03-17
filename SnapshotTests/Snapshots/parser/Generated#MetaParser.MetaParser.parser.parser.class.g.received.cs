93d56fc289494e5ae5d65525feca8058c1ffa1e1
David Sisco <dsisco11@gmail.com>
2023-03-17 16:26:54 -0700

    - remove lexer token defs which collide - add error test input for consumer start pattern collisions

--- SnapshotTests/Snapshots/parser/Generated#MetaParser.MetaParser.parser.parser.class.g.received.cs
+++ SnapshotTests/Snapshots/parser/Generated#MetaParser.MetaParser.parser.parser.class.g.received.cs
@@ -18,7 +18,7 @@ namespace UnitTestParser
             
             while (buffer2.Length > 0)
             {
-                if (TryProcessingLexer(buffer2, out var outId, out var outLength))
+                if (TryProcessingLexerToken(buffer2, out var outId, out var outLength))
                 {
                     if (buffer1.Length != buffer2.Length)
                     {
@@ -65,7 +65,7 @@ namespace UnitTestParser
             
             while (buffer3.Length > 0)
             {
-                if (TryProcessingSyntax(buffer3, out var outId, out var outLength))
+                if (TryProcessingSyntaxToken(buffer3, out var outId, out var outLength))
                 {
                     var consumed = buffer1.Slice(0, outLength).ToArray();
                     results.Add(new Token((ETokenType) outId, consumed) );
