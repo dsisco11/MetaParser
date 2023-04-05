a5e49b30ba7a3e5539a4c6b3abee2d3fd87ae567
David Sisco <dsisco11@gmail.com>
2023-04-05 00:06:38 -0700

    fix generation of duplicate TokenID constants

--- MetaParser/Builders/TokenIDConstBuilder.cs
+++ MetaParser/Builders/TokenIDConstBuilder.cs
@@ -1,6 +1,8 @@
 ﻿using MetaParser.Builders.Interfaces;
 using MetaParser.Core;
 
+using System;
+using System.Collections.Generic;
 using System.Collections.Immutable;
 
 namespace MetaParser.Builders;
@@ -10,13 +12,36 @@ internal class TokenIDConstBuilder : MetaCodeBuilder
 {
     protected override void Write(ParserContext context)
     {
-        var writer = context.Writer;
+        var writer = context.Writer ?? throw new InvalidOperationException("Writer is null");
         writer.WriteLine($"public const {context.Config.IdType} {Format_Token_Id(UnknownToken)} = 0;");
 
-        foreach (var token in context.Registry.Tokens.ToImmutableSortedSet())
+        int idTracker = 1;
+        Dictionary<string, int> tokenIds = new Dictionary<string, int>();
+        // add all registry tokens to the tokenId dictionary by their ID
+        foreach (var token in context.Registry.Tokens)
         {
-            writer.WriteLine($"public const {context.Config.IdType} {Format_Token_Id(token.ID)} = {token.Index};");
-            writer.WriteLine($"public const {context.Config.IdType} {Format_Token_Id(token.Name)} = {token.Index};");
+            string key = Format_Token_Id(token.ID);
+            if (!tokenIds.ContainsKey(key))
+            {
+                tokenIds.Add(key, idTracker++);
+            }
+        }
+        // first, write out all of the major token ID constants
+        foreach (var token in tokenIds)
+        {
+            writer.WriteLine($"public const {context.Config.IdType} {token.Key} = {token.Value};");
+        }
+
+        // next, write out all of the token name constants
+        var tokens = context.Registry.Tokens.ToImmutableSortedSet();
+
+        foreach (var token in tokens)
+        {
+            var id = Format_Token_Id(token.ID);
+            var name = Format_Token_Id(token.Name);
+            var index = tokenIds[id];
+
+            writer.WriteLine($"public const {context.Config.IdType} {name} = {index};");
         }
     }
 }
--- MetaParser/Builders/TokenIDEnumBuilder.cs
+++ MetaParser/Builders/TokenIDEnumBuilder.cs
@@ -1,7 +1,8 @@
 ﻿using MetaParser.Builders.Interfaces;
 using MetaParser.Core;
 
-using System.Collections.Immutable;
+using System;
+using System.Linq;
 
 namespace MetaParser.Builders;
 using static CodeCommon;
@@ -10,13 +11,13 @@ internal class TokenIDEnumBuilder : MetaCodeBuilder
 {
     protected override void Write(ParserContext context)
     {
-        var writer = context.Writer;
+        var writer = context.Writer ?? throw new InvalidOperationException("Writer is null");
         writer.WriteLine($"{Format_Token_Id(UnknownToken)} = ({context.Config.IdType}) 0,");
 
-        foreach (var token in context.Registry.Tokens.ToImmutableSortedSet())
+        foreach (var token in context.Registry.Tokens.Select(static (t) => Format_Token_Id(t.ID)).Distinct())
         {
-            var enumName = Format_Token_Id(token.ID);
-            writer.WriteLine($"{enumName} = ({context.Config.IdType}) {token.Index},");
+            var enumName = Format_Token_Id(token);
+            writer.WriteLine($"{enumName},");
         }
     }
 }
